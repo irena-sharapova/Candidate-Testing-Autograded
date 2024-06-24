f2b43532cb157f2f7f7482ffcbf2cad14efb89b0
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-06-24 14:24:51 -0400

    Task 2. Multiple Questions Added

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,8 +11,21 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride", 
+  "true", 
+  "40", 
+  "Trajectory", 
+  "3"
+];
+
 let candidateAnswers;
 
 
@@ -23,20 +36,36 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    candidateAnswer = input.question(question);
-}
+  //candidateAnswer = input.question(question);
 
-function gradeQuiz(candidateAnswers) {
-  if (candidateAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-        console.log(" Your Answer is Correct!");
-    } else {
-        console.log("Incorrect. The correct answer is " + correctAnswer + ".");
+  // if (candidateAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
+  //       console.log(" Your Answer is Correct!");
+  //   } else {
+  //       console.log("Incorrect. The correct answer is " + correctAnswer + ".");
+  //   }
+    
+    // let questions = 
+    
+    //let correctAnswers = 
+   
+    let candidateAnswers = [];
+
+    console.log(`\nCandidate name: ${candidateName}`);
+    for (let i = 0; i < questions.length; i++) {
+      let userResponse = input.question(`${questions[i]} `);
+      candidateAnswers.push(userResponse);
     }
-
+    
+    for (let i = 0; i < questions.length; i++) {
+      console.log(`${i + 1}) ${questions[i]}`);
+      console.log(`Your Answer: ${candidateAnswers[i]}`);
+      console.log(`Correct Answer: ${correctAnswers[i]}`);
+      console.log(""); 
+    }
+  }
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  function gradeQuiz(candidateAnswers) {
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -47,10 +76,10 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName + "!");
+  console.log("Hello, " + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+}  
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
