b3ac513cd379699d0d3126283f16c5d174f0ca01
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-06-29 12:26:33 -0400

    Part 3. Grade the Quiz Added.

--- candidate-testing.js
+++ candidate-testing.js
@@ -26,7 +26,7 @@ let correctAnswers = [
   "3"
 ];
 
-let candidateAnswers;
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -37,37 +37,48 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   //candidateAnswer = input.question(question);
-
   // if (candidateAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
   //       console.log(" Your Answer is Correct!");
   //   } else {
   //       console.log("Incorrect. The correct answer is " + correctAnswer + ".");
   //   }
-    
     // let questions = 
-    
     //let correctAnswers = 
    
-    let candidateAnswers = [];
+
  
    for (let i = 0; i < questions.length; i++) {
       let userResponse = input.question(`${questions[i]} `);
       candidateAnswers.push(userResponse);
     } 
+  }
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
+
+  function gradeQuiz(candidateAnswers) {
+    let numberOfCorrectAnswers = 0;
+
+    for (let i = 0; i < candidateAnswers.length; i++) {
+      if (candidateAnswers[i].trim().toLowerCase() === correctAnswers[i].toLowerCase()) {
+        numberOfCorrectAnswers += 1;
+      }
+    }
+
     console.log(`\nCandidate name: ${candidateName}`);
     for (let i = 0; i < questions.length; i++) {
       console.log(`${i + 1}) ${questions[i]}`);
       console.log(`Your Answer: ${candidateAnswers[i]}`);
       console.log(`Correct Answer: ${correctAnswers[i]}`);
       console.log(""); 
-    }
+    } 
+
+  let grade = (numberOfCorrectAnswers / questions.length) * 100;  //TODO 3.2 use this variable to calculate the candidates score.
+  console.log (`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${questions.length} responses correct) <<<`);
+
+  if (grade >= 80) {
+    console.log ('>>> Status: PASSED <<<');
+  }   else {
+    console.log ('>>> Status: FAILED <<<');
   }
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
-
-  function gradeQuiz(candidateAnswers) {
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
 
   return grade;
 }
